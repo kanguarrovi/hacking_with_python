e0ca898fe3fe2dcf53a58009d73417022d870af7
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2020-12-15 22:12:45 -0600

    Basics

---
+++ chapter_2_basics/tcp_client.py
@@ -0,0 +1,20 @@
+import socket
+
+target_host = "www.google.com"
+target_port = 80
+
+# create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect to the client
+client.connect((target_host, target_port))
+
+# send some data
+request = "GET / HTTP/1.1\r\nHost:%s\r\n\r\n" % target_host
+client.send(request.encode())
+
+# receive some data
+response = client.recv(4096)
+
+print(response)
+
